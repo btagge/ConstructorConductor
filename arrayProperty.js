f9ad2f81c9ffa6675cddeb2fc728908b44f8ff1f
Bryan Tagge <btagge@gmail.com>
2017-02-01 19:53:34 -0700

    Closures Practice

--- arrayProperty.js
+++ arrayProperty.js
@@ -3,3 +3,10 @@
 //Add a reverse method to the String 'class' so that every instance of String can call reverse and reverse itself.
   
   //code here
+
+  String.prototype.reverse = function() {
+    var reversed = this.split('').reverse().join('');
+    return reversed;
+  }
+
+  
